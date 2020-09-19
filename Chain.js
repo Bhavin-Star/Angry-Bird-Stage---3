c8d6b00a815795d526d8d49456599e95c49b4075
Bhavin-Star <70592387+Bhavin-Star@users.noreply.github.com>
2020-09-19 20:57:41 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,21 @@
+class Chain{
+    
+    constructor(bodyA, bodyB){
+        var options ={
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.004,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+    
+        World.add(world,this.chain);
+    }
+
+    display(){
+        strokeWeight(3);
+        line(this.chain.bodyA.position.x, this.chain.bodyA.position.y, this.chain.bodyB.position.x, this.chain.bodyB.position.x);
+
+    }
+
+}
